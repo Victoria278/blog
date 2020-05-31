58e6b5c99c749e4b3763604e3966ae69f94b29f2
Victoria278 <vika.frontovsky@gmail.com>
2020-05-31 22:30:41 +0300

    commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,33 @@
+@extends('admin.layout')
+
+@section('content')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Привет! Это панель администратора
+        <small></small>
+      </h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title">Главная страница</h3>
+        </div>
+        <div class="box-body">
+          Текст инструкции по пользованию админкой
+        </div>
+        <!-- /.box-body -->
+
+        <!-- /.box-footer-->
+      </div>
+      <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+@endsection
